64235727c7800912cade527d7c2ad79a38b26a38
Mark Viti <+>
2022-06-08 14:28:46 -0400

    commit tree 8aff7a18f8e4f934666f66583eb93b23e47fcf69

--- lib/student.rb
+++ lib/student.rb
@@ -1,6 +1,35 @@
 class Student
+  attr_accessor :name, :grade
+  attr_reader :id
 
-  # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  def initialize(name, grade, id=nil)
+    @name = name 
+    @grade = grade 
+    @id = id
+  end 
+
+  def self.create_table
+    sql<<-SQL
+      CREATE TABLE students (
+        id INTEGER PRIMARY KEY, 
+        name TEXT, 
+        grade REAL
+      )
+      SQL
+    DB[:conn].execute(sql) 
+  end 
+
+  # def self.create(name:, grade:, id: = nil)
+  #   sql = <<-SQL
+  #     INSERT INTO students (name, grade, id)
+  #     VALUES (name, grade, id)
+  #   SQL
+
+  #   DB[:conn]    
+
+
+  # # Remember, you can access your database connection anywhere in this class
+  # #  with DB[:conn] 
+  # end 
   
 end
